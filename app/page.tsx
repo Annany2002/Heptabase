673c3cb0d503ed5494fe0a4f0b09c93f23049df6
Annany Vishwakarma <annany.21211@knit.ac.in>
2024-07-07 17:28:48 +0530

    feat:scoping data to user idetifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 
 export default function Home() {
+  const documents = useQuery(api.documents.getDocuments);
+  const createDocument = useMutation(api.documents.createDocument);
+
   return (
-    <main>
+    <main className="flex flex-col justify-center items-center min-h-screen">
       <Unauthenticated>
         <SignInButton />
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <button onClick={() => createDocument({ title: "Hello world" })}>
+          Click Me
+        </button>
+        {documents?.map((doc) => <div key={doc._id}>{doc.title}</div>)}
       </Authenticated>
     </main>
   );
---
+++ convex/schema.ts
@@ -0,0 +1,11 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  documents: defineTable({
+    title: v.string(),
+    tokenIdentifier: v.string(),
+
+    //using index fast querying
+  }).index("by_tokenIdentifier", ["tokenIdentifier"]),
+});
