e6459069264e88a6495dbad6e954df18d3f947f4
Annany Vishwakarma <annany.21211@knit.ac.in>
2024-07-07 14:58:56 +0530

    feat: added clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated } from "convex/react";
+
 export default function Home() {
-  return <main>HeptaBase</main>;
+  return (
+    <main>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />
+      </Authenticated>
+    </main>
+  );
 }
