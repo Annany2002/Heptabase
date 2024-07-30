a3b56a352868f353d70ee83b9ca4951fbf94b14c
Annany Vishwakarma <annany.21211@knit.ac.in>
2024-07-30 23:44:57 +0530

    app completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,61 @@
+"use client";
+
+import { SignInButton } from "@clerk/nextjs";
+import Image from "next/image";
+
 export default function LandingPage() {
-  return <div>Landing Page</div>;
+  return (
+    <div className="bg-slate-100 min-h-screen">
+      <div className="relative isolate pt-10 px-6 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-12">
+          <div className="text-center space-y-4">
+            <Image
+              src={"/undraw_add_files_re_v09g.svg"}
+              width="200"
+              height="200"
+              alt="image"
+              className="mx-auto"
+            />
+            <h1 className="text-4xl dark:text-white font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Take control over your document like never before
+            </h1>
+            <p className="mt-6 text-lg dark:text-gray-200 leading-8 text-gray-600">
+              Heptabase is tool that allows you to chat with your pdf, make
+              notes, search 56 percent faster among all over your stuff with
+              vector search
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <button className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <SignInButton>Get started</SignInButton>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
